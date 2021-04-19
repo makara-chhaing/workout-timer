7b620e89fd876c946ac24adb968738a2e61a6c72
makara-chhaing <makara-chhaing@gmail.com>
2021-04-19 19:24:42 +1000

    implementing functionality

--- app/src/main/java/com/example/workouttimer/MainActivity.java
+++ app/src/main/java/com/example/workouttimer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.workouttimer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
@@ -13,6 +14,7 @@ import android.widget.Chronometer;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.sql.Time;
 import java.util.Locale;
@@ -26,11 +28,13 @@ public class MainActivity extends AppCompatActivity {
     ImageButton btn_play, btn_pause, btn_stop;
     EditText et_workout_type;
     TextView tv_previous;
+    long pauseDuration = 0, totalTime = -3;
     public static final String MY_PREFS = "MyPrefs";
-    String workout_type, time;
-    long pauseDuration;
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
     Chronometer chronometer;
-    Boolean isRunning = false;
+    Boolean isRunning = false, reset = false;
+    String type;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,10 +47,57 @@ public class MainActivity extends AppCompatActivity {
         chronometer = findViewById(R.id.cm_timer_id);
         tv_previous = findViewById(R.id.tv_previous_id);
 
-        SharedPreferences preferences = this.getSharedPreferences(MY_PREFS, MODE_PRIVATE);
-        workout_type = preferences.getString("type", "undefined");
-        time = preferences.getString("time", "undefined");
+        type = et_workout_type.getText().toString();
 
+        if(savedInstanceState != null){
+            isRunning = savedInstanceState.getBoolean("isRunning", false);
+            if(isRunning) {
+                pauseDuration = savedInstanceState.getLong("pause", 0);
+                chronometer.setBase(savedInstanceState.getLong("base_time", 0));
+                Log.d("Save168", "base time: " + savedInstanceState.getLong("base_time", 0));
+                Log.d("Save168", "pause: " + savedInstanceState.getLong("pause", 0));
+                Log.d("Save168", "is running: " + savedInstanceState.getBoolean("isRunning", false));
+                chronometer.start();
+            }
+            else {
+                pauseDuration = savedInstanceState.getLong("pause", 0);
+                chronometer.setBase(SystemClock.elapsedRealtime() - pauseDuration);
+            }
+        }
+
+        preferences = this.getSharedPreferences(MY_PREFS, MODE_PRIVATE);
+        type = preferences.getString("type", "");
+        totalTime = preferences.getLong("time", 0);
+        String time = String.format("%02d:%02d", totalTime/60, totalTime%60);
+        tv_previous.setText("You spent " + time +  " on " + type + " last time");
+
+        chronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+            @Override
+            public void onChronometerTick(Chronometer chronometer) {
+                if(reset){totalTime = -3; reset = false;}
+                totalTime++;
+                Log.d("Save168", "total time: " + totalTime);
+            }
+       });
+
+   }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if(isRunning){
+            outState.putLong("base_time", chronometer.getBase());
+            outState.putLong("pause", pauseDuration);
+            outState.putBoolean("isRunning", isRunning);
+        }else {
+        outState.putLong("pause", pauseDuration);
+        outState.putBoolean("isRunning", isRunning);
+        }
+
+        editor = this.getSharedPreferences(MY_PREFS, MODE_PRIVATE).edit();
+        editor.putString("type" , et_workout_type.getText().toString());
+        editor.putLong("time", totalTime);
+        editor.apply();
 
     }
 
@@ -67,7 +118,11 @@ public class MainActivity extends AppCompatActivity {
     public void stop(View v) {
         if(!isRunning){
             chronometer.setBase(SystemClock.elapsedRealtime());
+            String time = String.format("%02d:%02d", totalTime/60, totalTime%60);
+            tv_previous.setText("You spent " + time +  " on " + et_workout_type.getText().toString() + " last time");
             pauseDuration = 0;
+            reset = true;
+
         }
     }
 }
